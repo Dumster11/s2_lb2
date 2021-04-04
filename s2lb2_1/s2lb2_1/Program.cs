8605a299ef9386e2c586bc21627f3b3b6d536b4c
Dumster11 <71654055+Dumster11@users.noreply.github.com>
2021-04-04 18:05:49 +0300

    f

---
+++ s2lb2_1/s2lb2_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace s2lb2_1
+{
+    class MyList<T>
+    {
+        private T[] myList = null;
+
+        public T this[int index]
+        {
+            get { return myList[index]; }
+            set { myList[index] = value; }
+        }
+
+        public MyList()
+        {
+            this.myList = new T[1];
+        }
+
+        public MyList(int count)
+        {
+            this.myList = new T[count];
+        }
+
+        public void Add(T item)
+        {
+            T[] extendedList = new T[myList.Length + 1];
+            extendedList[extendedList.Length - 1] = item;
+            myList = extendedList;
+        }
+
+        public int Capacity
+        {
+            get { return myList.Length; }
+
+        }
+
+       
+
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < myList.Length; i++)
+                {
+                    if (myList[i].ToString() != null)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> mylist = new MyList<int>(5);
+            mylist[0] = 1;
+            Console.WriteLine("Емкость списка: {0} элемент(-ов)", mylist.Capacity);
+            Console.WriteLine("Список фактически содержит: {0} элемент(-ов)", mylist.Count);
+            mylist.Add(15);
+            Console.WriteLine("Емкость списка: {0} элемент(-ов)", mylist.Capacity);
+
+            Console.ReadKey();
+        }
+    }
+}
